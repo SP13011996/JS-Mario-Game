ccf25176b9a3fef32667c54296d4cd60946ed889
sp13011996 <githubprojects0508@gmail.com>
2021-10-16 11:48:29 +0530

    Added physics ,Collision,sprite

--- game.js
+++ game.js
@@ -5,7 +5,15 @@ let config={
         width:800,
         height:600,        
     },
-    backgroundColor:0xff00cc,
+    physics:{
+        default:'arcade',
+        arcade:{
+            gravity:{
+                y:1000
+            }
+        }
+    },
+    backgroundColor:0xffffcc,
     scene:{
         preload:preload,
         create:create,
@@ -17,10 +25,35 @@ let config={
 let game=new Phaser.Game(config)
 
 function preload(){
-    this.load.image("ground","Assets/top-ground.jpg")
+    this.load.image("ground","Assets/top-ground.png")
+    this.load.image("background","Assets/sky.png")
+    this.load.spritesheet("dude","Assets/dude.png",{frameWidth:32,frameHeight:48})
 }
 function create(){
-    
+    var W=game.config.width;
+    var H=game.config.height
+
+    //Adding ground
+    let ground=this.add.tileSprite(0,H-128,W,128,'ground')
+    ground.setOrigin(0,0)
+
+    //Adding background
+    let background=this.add.sprite(0,0,"background")
+    background.setOrigin(0,0)
+    background.displayWidth=W
+    background.displayHeight=H
+    background.depth=-1
+
+    //Adding Player
+    let player=this.physics.add.sprite(100,100,'dude',4)
+
+
+    this.physics.add.existing(ground,true)
+    //ground.body.allowGravity=false
+    //ground.body.immovable=true;
+
+    this.physics.add.collider(ground,player)
+
 }
 function update(){
     
